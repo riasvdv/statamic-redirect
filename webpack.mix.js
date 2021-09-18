e49e3e1d8df10b5d8080ca00f88bbc83bee3d62f
Rias <hey@rias.be>
2021-09-18 10:31:21 +0200

    Laravel mix 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require("laravel-mix");
 
 mix
-  .js("resources/js/cp.js", "resources/dist/js")
+  .js("resources/js/cp.js", "resources/dist/js").vue()
   .setPublicPath("resources/dist");
