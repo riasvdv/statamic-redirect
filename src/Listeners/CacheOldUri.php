58c8e42ea42677730fe4465e1f4bbe0ca4a55169
Ryan Mitchell <ryan@thoughtcollective.com>
2023-11-30 19:24:04 +0000

    Clear eloquent blink cache in cache old uri listener

--- src/Listeners/CacheOldUri.php
+++ src/Listeners/CacheOldUri.php
@@ -4,6 +4,7 @@ namespace Rias\StatamicRedirect\Listeners;
 
 use Illuminate\Support\Facades\Cache;
 use Statamic\Events\EntrySaving;
+use Statamic\Facades\Blink;
 use Statamic\Facades\Entry;
 
 class CacheOldUri
@@ -14,7 +15,10 @@ class CacheOldUri
             return;
         }
         
-        $entry = Entry::find($entrySaving->entry->id());
+        $id = $entrySaving->entry->id();
+        
+        Blink::forget('eloquent-entry-'.$id);
+        $entry = Entry::find($id);
 
         if (! $entry || ! $uri = $entry->uri()) {
             return;
