93f80b8117937a0dc5894f563035fac0dd189e5f
Rias <hey@rias.be>
2025-08-01 22:31:39 +0200

    Reorder redirects

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,9 +1,7 @@
-//import PublishForm from "./components/Publish/PublishForm";
 import RedirectListing from "./components/Listing/RedirectListing.vue";
 import ErrorsListing from "./components/Listing/ErrorsListing.vue";
 
 Statamic.booting(() => {
-    //Statamic.$components.register("redirect-publish-form", PublishForm);
     Statamic.$components.register("redirect-listing", RedirectListing);
     Statamic.$components.register("errors-listing", ErrorsListing);
 });
--- resources/views/redirects/index.blade.php
+++ resources/views/redirects/index.blade.php
@@ -2,36 +2,6 @@
 @section('title', Statamic::crumb("Redirect", "Redirects"))
 
 @section('content')
-    <ui-header title="{{ __('Redirects') }}" icon="arrow-up-right">
-
-        @can('create', \Rias\StatamicRedirect\Contracts\Redirect::class)
-            <ui-dropdown>
-                <ui-dropdown-menu>
-                    <ui-dropdown-item
-                        icon="upload"
-                        text="Import CSV"
-                        href="{{ cp_route('redirect.redirects.import') }}"
-                    ></ui-dropdown-item>
-                    <ui-dropdown-item
-                        icon="download"
-                        text="Export as CSV"
-                        href="{{ cp_route('redirect.export', ['type' => 'csv']) }}?download=true"
-                    ></ui-dropdown-item>
-                    <ui-dropdown-item
-                        icon="download"
-                        text="Export as JSON"
-                        href="{{ cp_route('redirect.export', ['type' => 'json']) }}?download=true"
-                    ></ui-dropdown-item>
-                </ui-dropdown-menu>
-            </ui-dropdown>
-            <ui-button
-                href="{{ cp_route('redirect.redirects.create') }}"
-                text="{{ __('Create Redirect') }}"
-                variant="primary"
-            />
-        @endcan
-    </ui-header>
-
     <redirect-listing
         :columns="{{ json_encode($columns) }}"
         :filters="{{ json_encode($filters) }}"
