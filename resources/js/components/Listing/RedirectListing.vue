899f46718707ef232538405bbf6a17ffc77c4fa4
Rias <hey@rias.be>
2025-08-01 22:31:39 +0200

    Reorder redirects

--- resources/js/components/Listing/RedirectListing.vue
+++ resources/js/components/Listing/RedirectListing.vue
@@ -1,49 +1,98 @@
 <script setup>
-import { StatusIndicator, DropdownItem, Listing } from '@statamic/ui';
+import { StatusIndicator, DropdownItem, Listing, Header } from '@statamic/ui';
 import { ref } from 'vue';
+import axios from 'axios';
 
 const props = defineProps({
     actionUrl: String,
     columns: Array,
     filters: Array,
+    reordering: Boolean,
 });
 const preferencesPrefix = ref(`redirect.redirects`);
 const requestUrl = ref(cp_url(`redirect/api/redirects`));
 const items = ref(null);
 const page = ref(null);
 const perPage = ref(null);
+const view = ref('list');
 
 function requestComplete({ items: newItems, parameters }) {
   items.value = newItems;
   page.value = parameters.page;
   perPage.value = parameters.perPage;
 }
+
+function reordered(value) {
+  axios.post(cp_url(`redirect/api/redirects/reorder`), {
+    redirects: value,
+  }).then(() => {
+    Statamic.$toast.success(__('Redirects re-ordered'));
+  }).catch(() => {
+    Statamic.$toast.error(__('Something went wrong'));
+  });
+}
 </script>
 
 <template>
-    <Listing
-        ref="listing"
-        :url="requestUrl"
-        :columns="columns"
-        :action-url="actionUrl"
-        :preferences-prefix="preferencesPrefix"
-        :filters="filters"
-        push-query
-        @request-completed="requestComplete"
-    >
-        <template v-slot:[`cell-source`]="{ row: redirect }">
-            <a class="title-index-field" :href="redirect.edit_url" @click.stop>
-                <StatusIndicator :status="redirect.enabled ? 'published' : 'hidden'" />
-                <span v-text="redirect.source" />
-            </a>
-        </template>
-        <template #prepended-row-actions="{ row: redirect }">
-            <DropdownItem
-                v-if="redirect.editable"
-                :text="__('Edit')"
-                :href="redirect.edit_url"
-                icon="edit"
-            />
-        </template>
-    </Listing>
+    <div>
+      <Header title="Redirects" icon="arrow-up-right">
+        <ui-toggle-group v-model="view">
+          <ui-toggle-item icon="navigation" value="tree" />
+          <ui-toggle-item icon="layout-list" value="list" />
+        </ui-toggle-group>
+
+        <ui-dropdown>
+          <ui-dropdown-menu>
+            <ui-dropdown-item
+                icon="upload"
+                text="Import CSV"
+                :href="cp_url('redirect/redirects/import')"
+            ></ui-dropdown-item>
+            <ui-dropdown-item
+                icon="download"
+                text="Export as CSV"
+                :href="cp_url('redirect/export/csv?download=true')"
+            ></ui-dropdown-item>
+            <ui-dropdown-item
+                icon="download"
+                text="Export as JSON"
+                :href="cp_url('redirect/export/json?download=true')"
+            ></ui-dropdown-item>
+          </ui-dropdown-menu>
+        </ui-dropdown>
+        <ui-button
+            :href="cp_url('redirect/redirects/create')"
+            text="Create Redirect"
+            variant="primary"
+        />
+      </Header>
+      <Listing
+          ref="listing"
+          :url="requestUrl"
+          :columns="columns"
+          :action-url="actionUrl"
+          :preferences-prefix="preferencesPrefix"
+          :filters="filters"
+          :filters-for-reordering="filtersForReordering"
+          :reorderable="view === 'tree'"
+          push-query
+          @request-completed="requestComplete"
+          @reordered="reordered"
+      >
+          <template v-slot:[`cell-source`]="{ row: redirect }">
+              <a class="title-index-field" :href="redirect.edit_url" @click.stop>
+                  <StatusIndicator :status="redirect.enabled ? 'published' : 'hidden'" />
+                  <span v-text="redirect.source" />
+              </a>
+          </template>
+          <template #prepended-row-actions="{ row: redirect }">
+              <DropdownItem
+                  v-if="redirect.editable"
+                  :text="__('Edit')"
+                  :href="redirect.edit_url"
+                  icon="edit"
+              />
+          </template>
+      </Listing>
+    </div>
 </template>
